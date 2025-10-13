a29980b13194e8138a08880dc161b48be0bba393
miles <719713496@qq.com>
2025-10-13 15:27:16 +0800

    Change empty state icon to minus symbol for clearer empty indication
    - Replace play icon with minus symbol (HiMinus)
    - Make background more transparent (bg-gray-800/30)
    - Use darker gray color (text-gray-500) for better empty state indication
    - Minus symbol clearly represents 'empty' or 'no content' state
    - More intuitive than play button for empty state

--- frontend/src/components/EmptyPlayer.tsx
+++ frontend/src/components/EmptyPlayer.tsx
@@ -1,12 +1,12 @@
-import { HiPlay } from "react-icons/hi";
+import { HiMinus } from "react-icons/hi";
 
 interface IProps {}
 
 export default function EmptyPlayer(props: IProps) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <div className="w-16 h-16 rounded-full bg-gray-800/50 flex items-center justify-center border border-gray-600/50">
-        <HiPlay className="text-2xl text-gray-400" />
+      <div className="w-16 h-16 rounded-full bg-gray-800/30 flex items-center justify-center border border-gray-600/30">
+        <HiMinus className="text-3xl text-gray-500" />
       </div>
     </div>
   );
