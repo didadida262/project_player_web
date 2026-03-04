ae2bae7733882165267bd434660d3e51f4848eac
miles <719713496@qq.com>
2026-03-04 23:11:17 +0800

    some_change

--- frontend/src/components/EmptyPlayer.tsx
+++ frontend/src/components/EmptyPlayer.tsx
@@ -1,12 +1,12 @@
-import { HiMinus } from "react-icons/hi";
+import { HiOutlineFolderOpen } from "react-icons/hi";
 
 interface IProps {}
 
 export default function EmptyPlayer(props: IProps) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <div className="w-16 h-16 rounded-full bg-gray-800/30 flex items-center justify-center border border-gray-600/30">
-        <HiMinus className="text-3xl text-gray-500" />
+      <div className="w-20 h-20 rounded-full bg-gray-800/10 flex items-center justify-center border border-dashed border-gray-600/50">
+        <HiOutlineFolderOpen className="text-4xl text-gray-400" />
       </div>
     </div>
   );
